e374c7d8cd740ed78f881cf2ce634a794e66070d
Sone <sthaya@gmail.com>
2021-04-18 15:52:08 -0700

    shipping and clear filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -134,7 +134,22 @@ const Filters = () => {
             />
           </div>
           {/* end price */}
+          {/* start shipping */}
+          <div className="form-control shipping">
+            <label htmlFor="shipping">free shipping</label>
+            <input
+              type="checkbox"
+              name="shipping"
+              id="shipping"
+              onChange={updateFilters}
+              checked={shipping}
+            />
+          </div>
+          {/* end shipping */}
         </form>
+        <button type="button" className="clear-btn" onClick={clearFilters}>
+          clear filters
+        </button>
       </div>
     </Wrapper>
   );
