ac9df7542b6390138560ff50ffe56216e268cf5b
Sone <sthaya@gmail.com>
2021-04-17 18:59:45 -0700

    filters text

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -18,10 +18,10 @@ const initialState = {
   grid_view: true,
   sort: "price-lowest",
   filters: {
-    text: '',
-    company: 'all',
-    category: 'all',
-    color: 'all',
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
     min_price: 0,
     max_price: 0,
     price: 0,
@@ -40,8 +40,9 @@ export const FilterProvider = ({ children }) => {
   }, [products]);
 
   useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS });
     dispatch({ type: SORT_PRODUCTS });
-  }, [products, state.sort]);
+  }, [products, state.sort, state.filters]);
 
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW });
@@ -58,9 +59,24 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: UPDATE_SORT, payload: value });
   };
 
+  const updateFilters = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
+  };
+
+  const clearFilters = () => {};
+
   return (
     <FilterContext.Provider
-      value={{ ...state, setGridView, setListView, updateSort }}
+      value={{
+        ...state,
+        setGridView,
+        setListView,
+        updateSort,
+        updateFilters,
+        clearFilters,
+      }}
     >
       {children}
     </FilterContext.Provider>
